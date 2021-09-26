d5530570d12519d310327f21b63a3321abc6e2a4
dan <dan@leftcoastfs.com>
2021-09-25 18:10:09 -0700

    Setup to be pulled into severeless env

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='lcfsLambdaLib',
+    author='Dan Herrington',
+    author_email='dan@cascadiarc.com',
+    version='0.1',
+    packages=[
+        'lcfsLambdaLib',
+    ],
+    url='https://github.com/cascadiarc/lcfsLambdaLib',
+    license='MIT License',
+    description='Library to hold commonly used aws functions for Lambda',
+    long_description=open('README.md').read(),
+)
