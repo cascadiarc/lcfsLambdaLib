ed49f44576200ca16c31563ee0215db8c3131bf7
dan <dan@leftcoastfs.com>
2021-09-27 16:59:02 -0700

    Updated with version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='lcfsLambdaLib',
     author='Dan Herrington',
     author_email='dan@cascadiarc.com',
-    version='0.1',
+    version='0.2',
     packages=[
         'lcfsLambdaLib',
     ],
