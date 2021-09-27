fa58a0c3a838c37ad80aa4c31e21c439585a70a9
dan <dan@leftcoastfs.com>
2021-09-27 16:48:06 -0700

    Updated with sns

--- lcfsLambdaLib/lcfsLambdaLib.py
+++ lcfsLambdaLib/lcfsLambdaLib.py
@@ -1,4 +1,6 @@
 import boto3, json
+from types import MappingProxyType
+
 '''This file contains code reused in all our lambdas'''
 
 def get_ssm_params(logger,path,enc):
@@ -21,7 +23,9 @@ def get_ssm_params(logger,path,enc):
             logger.debug(f'Sucessfully pulled value from {i}')
             a[i] = param1['Parameter']['Value']
     logger.debug(f'SSM parameter(s) done')
-    return a
+
+    frozen_dict = MappingProxyType(a)
+    return frozen_dict
 
 def send_sqs_message(logger,sqs_queue_name, msg_att, msg_body):
     """
@@ -59,7 +63,7 @@ def send_sns_message(logger,topic_arn,message,m_struct,m_attr):
     :param5: m_attr=
     :return: repsonse=either the dict of messageId and SequenceNumber
     '''
-    
+
     logger.info(f'Calling {topic_arn} for message')
     logger.debug(f'Message: {message}')
     logger.debug(f'MessageStructure: {m_struct}')
@@ -79,4 +83,19 @@ def send_sns_message(logger,topic_arn,message,m_struct,m_attr):
     
     return response
 
+def read_s3_file(logger,bucket,key,tp):
+    '''
+    This function takes a bucket and a key and returns the data from the s3 file
+    :param1: bucket=the bucket name that the file resides in
+    :param2: key=the key to the file, which in s3 is the path and filename in one
+    :param3: type=thsi is the file type
+    :returns: contents=the output of the s3 read command
+    '''
+    # Get s3 object contents based on bucket name and object key; in bytes and convert to string
+    try:
+        data = s3_client.get_object(Bucket=bucket, Key=key)
+    except ClientError as e:
+        logger.info(f'Error reading {bucket}/{key}: {e.response["Error"]["Code"]}')
+    contents = data['Body'].read().decode("utf-8")    
 
+    return contents
